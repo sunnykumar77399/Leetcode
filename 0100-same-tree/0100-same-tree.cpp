a9d9450e2b8f9132c2dcd5c628d7f651076df762
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-09-01 13:25:53 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -16,9 +16,9 @@ public:
         if((q==NULL && p!=NULL) || (q!=NULL && p==NULL)) return false;
 
         if(p->val!=q->val) return false;
-        
-        else if(!check(p->left,q->left)) return false;
-        else if(!check(p->right,q->right)) return false;
+
+        if(!check(p->left,q->left)) return false;
+        if(!check(p->right,q->right)) return false;
         
         return true;
         
