b73c18f0af3511f8fde73e0b6408f107d5b46ee1
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-09-08 18:06:14 +0530

    Time: 6 ms (47.69%), Space: 23.8 MB (57.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> res;
-
         int start=intervals[0][0],end=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
             if(end>=intervals[i][0]){ 
@@ -15,7 +14,7 @@ public:
             }
 
         }
-                res.push_back({start,end});
+        res.push_back({start,end});
         return res;
     }
 };
